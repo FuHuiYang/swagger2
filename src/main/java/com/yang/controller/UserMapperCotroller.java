fd333fb4c09ac583727bef5c33275beeff134d28
yangfuhui <only_yangfuhui@163.com>
2019-10-11 10:15:00 +0800

    添加redis相关test

--- src/main/java/com/yang/controller/UserMapperCotroller.java
+++ src/main/java/com/yang/controller/UserMapperCotroller.java
@@ -1,5 +1,6 @@
 package com.yang.controller;
 
+import com.alibaba.druid.support.json.JSONUtils;
 import com.yang.bean.User;
 import com.yang.mybatis.mapper.UserMapper;
 import com.yang.mybatis.mapper1.UserMapperOne;
@@ -8,9 +9,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
 
 import java.util.List;
 
@@ -30,6 +34,8 @@ public class UserMapperCotroller {
     UserMapperOne userMapperOne;
     @Autowired
     UserMapperTwo userMapperTwo;
+    @Autowired
+    RedisTemplate redisTemplate;
 
 //    @RequestMapping("/selectUserMapper")
 //    public List<User> selectUserMapper(){
@@ -44,6 +50,19 @@ public class UserMapperCotroller {
     @RequestMapping("/selectUserMapperTwo")
     public List<User> selectUserMapperTwo(){
         List<User> userList = userMapperTwo.getAllUser();
+        return userList;
+    }
+
+    @RequestMapping("/selectUserMapperOneToRedis")
+    public List<User> selectUserMapperOneToRedis(){
+        List<User> userList = userMapperTwo.getAllUser();
+        ValueOperations ops = redisTemplate.opsForValue();
+        Object k1 = ops.get("userList");
+        if(null!=k1){
+            System.out.println(k1);
+        }
+        ops.set("userList", userList);
+        System.out.println(k1);
         return userList;
     }
 
